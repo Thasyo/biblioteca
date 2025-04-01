4baa4623391e14cbcc254e24214fa468a1109c35
Thasyo Peres <thasyotp2002@gmail.com>
2025-03-31 21:59:08 -0300

    atualizando arquivo index.php

--- App/index.php
+++ App/index.php
@@ -1,3 +1,10 @@
-<?php 
-    echo "Opan";
+<?php
+
+    use App\Controller\AlunoController;
+
+    include "config.php"; //importa todos os dados dentro do arquivo config.php
+    include "autoload.php";
+    include "routes.php";
+
+    new AlunoController();
 ?>
